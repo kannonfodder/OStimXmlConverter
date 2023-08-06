b7df48789cea60a4e991b6db01f955145d4bc08b
kannonfodder <kannonfodder@kannonfoundry.dev>
2023-08-06 15:24:20 +0100

    First pass

---
+++ XMLConverter/Program.cs
@@ -0,0 +1,130 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+using XMLConverter;
+
+Console.WriteLine("Hello, World!");
+var inputPath = "";
+var outputPath = "";
+if (args.Length < 1)
+{
+    Console.WriteLine("Enter input filepath");
+    inputPath = Console.ReadLine()!;
+}
+else
+{
+    inputPath = args[0];
+   
+}
+outputPath = inputPath;
+if (args.Length < 2)
+{
+    Console.Write("Enter output filepath");
+    var line = Console.ReadLine();
+    if (!string.IsNullOrEmpty(line))
+    {
+        outputPath = line;
+    }
+}
+
+var errors = new List<string>();
+//Grab XMLS
+var files = Directory.GetFiles(inputPath, "*.xml", new EnumerationOptions() { RecurseSubdirectories = true });
+var scenes = new Dictionary<string, Scene>();
+foreach (var file in files)
+{
+    Console.WriteLine($"Loading {file}");
+    
+    XmlDocument doc = new XmlDocument();
+    try
+    {
+        doc.Load(file);
+    }
+    catch (Exception ex)
+    {
+        errors.Add($"{file} : {ex.Message}");
+    }
+
+    if (doc["scene"] != null)
+    {
+        var scene = new Scene();
+        var sceneTag = doc["scene"];
+        scene.Name = sceneTag["info"]?.Attributes["name"]?.Value ?? doc["scene"]?.Attributes["id"]?.Value!;
+        //TODO: ModPack - Animator name?
+        scene.Length = Convert.ToSingle(sceneTag["anim"]?.Attributes["l"]?.Value);
+        //TODO: Handle Destination/Origin
+        if (sceneTag["nav"] != null)
+        {
+            for(var i = 0; i <sceneTag["nav"]?.ChildNodes.Count; i ++)
+            {
+                var tab = sceneTag["nav"]!.ChildNodes.Item(i);
+                
+                if (tab is not { Name: "tab" }) continue;
+                
+                for (var j = 0; j < tab.ChildNodes.Count; j++)
+                {
+                    var page = tab.ChildNodes.Item(j);
+                    if (page is not { Name: "page" }) continue;
+                    for (var k = 0; k < page.ChildNodes.Count; k++)
+                    {
+                        var option = page.ChildNodes.Item(k);
+                        if (option is not { Name: "option" }) continue;
+                        scene.Navigations.Add(new Navigation()
+                        {
+                            Destination = option.Attributes["go"].Value,
+                            //TODO: Convert navigation texts
+                            Description = option.Attributes["text"].Value
+                                
+                        });
+                    }
+                }
+            }
+        }
+
+        if (doc["metadata"] != null && doc["metadata"]?.Attributes["tags"] != null)
+        {
+            scene.Tags.AddRange(doc["metadata"]!.Attributes["tags"].Value.Split(','));
+        }
+        
+        
+        scenes.Add(file, scene);
+    }
+    else
+    {
+        Console.WriteLine($"{file} is not a scene xml");
+    }
+}
+
+
+
+//Write object to Json
+
+
+foreach (var (k, v) in scenes)
+{
+    var path = Path.Combine(outputPath,$"{Path.GetFileNameWithoutExtension(k)}.json");
+    Console.WriteLine($"Writing {path}");
+    var jsonString = JsonSerializer.Serialize(v, new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+        WriteIndented = true
+    });
+    File.WriteAllText(path, jsonString);
+}
+
+//output errors
+if (errors.Count == 0)
+{
+    Console.WriteLine("Success! All files converted");
+}
+else
+{
+    Console.WriteLine("Errors occurred during conversion process");
+    foreach (var error in errors)
+    {
+        Console.WriteLine(error);
+    }
+}
---
+++ XMLConverter/Scene.cs
@@ -0,0 +1,63 @@
+﻿namespace XMLConverter;
+
+public class Scene
+{
+    public string Name { get; set; }
+    public string ModPack { get; set; }
+    public float Length { get; set; }
+    public string? Destination { get; set; }
+    public string? Origin { get; set; }
+
+    public List<Navigation> Navigations { get; } = new List<Navigation>();
+    public List<Speed> Speeds { get; } = new List<Speed>();
+    public int DefaultSpeed { get; set; }
+    public bool NoRandomSelection { get; set; }
+    public string Furniture { get; set; } = "None";
+    public List<string> Tags { get; } = new List<string>();
+    public Dictionary<string, string> AutoTransitions { get; } = new Dictionary<string, string>();
+    public List<Actor> Actors { get; } = new List<Actor>();
+    public List<SceneAction> Actions { get; } = new List<SceneAction>();
+}
+
+public class Navigation
+{
+    public string Destination { get; set; }
+    public string Origin { get; set; }
+    public int Priority { get; set; }
+    public string Description { get; set; }
+    public string Icon { get; set; }
+    public string Border { get; set; }
+    public bool NoWarnings { get; set; }
+}
+
+public class Speed
+{
+    public string Animation { get; set; }
+    public float PlaybackSpeed { get; set; }
+    public float DisplaySpeed { get; set; }
+}
+
+public class Actor
+{
+    public string Type { get; set; } = "npc";
+    public string IntendedSex { get; set; } = "any";
+    public int SosBed { get; set; }
+    public float Scale { get; set; }
+    public float ScaleHeight { get; set; } = 120.748f;
+    public int ExpressionAction { get; set; }
+    public int Lookup { get; set; }
+    public int LookDown { get; set; }
+    public int LookLeft { get; set; }
+    public int LookRight { get; set; }
+    public List<string> Tags { get; set; }
+    public bool FeetOnGround { get; set; }
+    public Dictionary<string, string> AutoTransitions { get; } = new Dictionary<string, string>();
+}
+
+public class SceneAction
+{
+    public string Type { get; set; }
+    public int Actor { get; set; }
+    public int? Target { get; set; }
+    public int? Performer { get; set; }
+}
